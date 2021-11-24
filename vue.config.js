134eaeb6de51f8c4111336f8076cf693ce838f91
renmei97 <57889770+renmei97@users.noreply.github.com>
2021-11-24 10:39:22 +0800

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  outputDir: './bulid',
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://152.136.185.210:5000',
+        pathRewrite: {
+          '^/api': ''
+        },
+        changeOrigin: true
+      }
+    }
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        components: '@/components'
+      }
+    }
+  }
+}
